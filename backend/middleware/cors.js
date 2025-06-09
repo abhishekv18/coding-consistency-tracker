154326a623da32283fcf6a3f11dc9583bea99d2a
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2025-06-09 10:54:25 +0530

    change

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:5174', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000','http://localhost:5174'],
 
    // origin: ['http://localhost:5173', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
